b061e414ca18fd7c4cd40d65e880bbd8880d9019
Sudo-Si <56238070+Sudo-Si@users.noreply.github.com>
2021-05-03 09:11:03 +1200

    first commit

---
+++ models/subscriber.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const subscriberSchema = new mongoose.Schema({
+
+
+    name: {
+type:String, require:true
+    },
+    subscribertochannel:{
+        type:String, require:true
+    },
+    subscribeDate:{
+        type:Date, require:true, default:Date.now
+    }
+})
+module.exports = mongoose.model('subscriber', subscriberSchema)
